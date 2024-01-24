049c51d979dc3e17af69955e58e7ee3cfd166365
Paolo Insogna <paolo@cowtech.it>
2024-01-24 16:40:47 +0100

    chore: Updated TypeScript configuration.

--- src/internals.ts
+++ src/internals.ts
@@ -1,4 +1,4 @@
-import imageSize from 'image-size'
+import { imageSize } from 'image-size'
 import type EventEmitter from 'node:events'
 import { createReadStream } from 'node:fs'
 import { type IncomingHttpHeaders } from 'node:http'
